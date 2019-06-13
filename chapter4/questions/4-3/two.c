d79901c2c7ab555c82eb64264a61fa6630ea65e8
Jaap Huisman <jaap.huisman89@gmail.com>
2019-06-13 17:21:02 +0100

    Questions chapter 4

---
+++ chapter4/questions/4-3/two.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int answer;
+
+int main()
+{
+	answer = 2 + 2;
+
+	printf("The answer id %d\n");
+
+	return 0;
+}
---
+++ chapter4/questions/4-4/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float result;
+
+int main()
+{
+	result = 7.0 / 22.0;
+
+	printf("The result id %d\n", result);
+
+	return 0;
+}
