8996d447e504d55f2a49dd02184beff89631f91f
Jaap Huisman <jaap.huisman89@gmail.com>
2019-06-26 11:51:01 +0100

    11-4 count bits

---
+++ chapter11/11-4/count-bits.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int count_integer_bits(int integer);
+
+int main()
+{
+	int test_integer = 14; /* test integer to count bits */
+
+	printf("## sizeof(int) = %ld\n", sizeof(int));
+
+	printf("Number of bits set in %d: %d\n", test_integer, count_integer_bits(test_integer));
+
+	return 0;
+}
+
+int count_integer_bits(int integer)
+{
+	int number_of_bits = 0; /* initialize bit count */
+	int test_bit; /* bit to test, used in loop */
+
+	/* 
+	 * Loop through all bits of integer
+	 * Assumes 4 byte int
+	 */
+	for (test_bit = 0x8000; test_bit != 0; test_bit = (test_bit >> 1)) {
+		/* Test if bit is set */
+		if ((test_bit & integer) != 0)
+			++number_of_bits;
+	}
+
+	return number_of_bits;
+}
