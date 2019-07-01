af58b7510611e9adf6565b84095601cb82e0fe4c
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-01 11:04:10 +0100

    14-2 copy file expanding tabs spaces

--- chapter14/14-1/count-lines.c
+++ chapter14/14-1/count-lines.c
@@ -1,10 +1,27 @@
+/*
+ * Program: count_lines
+ *
+ * Purpose:
+ * 	Counts number of lines in a file
+ *
+ * Usage: 
+ * 	count_lines file
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int count_lines(char file_name[]);
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Line count: %d\n", count_lines("dummy.txt"));
+	if (argc == 1) {
+		fprintf(stderr, "Missing filename\n");
+		fprintf(stderr, "Usage: count_line <filename>\n");
+		return 1;
+	}
+
+	printf("Line count: %d\n", count_lines(argv[1]));
 
 	return 0;
 }
@@ -12,10 +29,17 @@ int main()
 int count_lines(char file_name[])
 {
 	FILE *in_file; /* file of which to count the lines */
-	int  lines;    /* number of lines so far */
-	int  ch;       /* current character */
 
 	in_file = fopen(file_name, "r");
+
+	if (in_file == NULL) {
+		fprintf(stderr, "Error: unable to open file '%s'\n", file_name);
+		exit(8);
+	}
+
+	int lines; /* number of lines so far */
+	int ch;    /* current character */
+
 	lines = 0;
 
 	for (ch = getc(in_file); ch != EOF; ch = getc(in_file)) {
---
+++ chapter14/14-2/copy-with-spaces.c
@@ -0,0 +1,108 @@
+/*
+ * Program: copy-with-spaces
+ *
+ * Purpose:
+ * 	Copies a file and replaces all tabs by multiple spaces
+ *
+ * Usage:
+ * 	copy-with-spaces [options] <filename> <new filename>
+ *
+ * Options
+ *  -s<spaces> 	sets number of spaces
+ *  			(default=4)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE (16 * 1024) /* 16K buffer */
+
+char src_filename[20];    /* filename source file to be copied */
+char dest_filename[20];   /* destination filename */
+int  spaces = 4;          /* number of spaces for tab replacement, default 4 */
+
+void usage(void);
+
+int main(int argc, char *argv[])
+{
+	int  ch;            /* character of input file */
+	int  space_counter; /* used in for loop to replace tab by correct number of spaces */
+	FILE *source_ptr;   /* source file pointer */
+	FILE *dest_ptr;     /* destination file pointer */
+
+	/* Process optional -s option */
+	if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 's') {
+		/* Save given spaces value */
+		sscanf(&(argv[1][2]), "%d", &spaces);
+
+		/* 
+		 * Process option
+		 * 1) move argument list up one
+		 * 2) move argument count down one
+		 */
+		++argv;
+		--argc;
+	}
+
+	/* Check for required filename parameters */
+	if (argc < 3) {
+		fprintf(stderr, "Error: Missing filename(s)\n");
+		usage();
+		exit(8);
+	}
+
+	/* 
+	 * Syntax okay
+	 * Set source and destination
+	 */
+	strcpy(src_filename, argv[1]);
+	strcpy(dest_filename, argv[2]);
+
+	source_ptr = fopen(src_filename, "r");
+	dest_ptr = fopen(dest_filename, "w");
+
+	if (source_ptr == NULL) {
+		fprintf(stderr, "Can not open source file %s\n", src_filename);
+		exit(8);
+	}
+
+	if (dest_ptr == NULL) {
+		fprintf(stderr, "Can not open destination file %s\n", dest_filename);
+		exit(8);
+	}
+
+	/* Loop through file characters one by one */
+	while (1) {
+		ch = fgetc(source_ptr);
+
+		if (ch == EOF)
+			break;
+
+		/* Check if character is tab */
+		if (ch == '\t') {
+			/* Replace tab by spaces */
+			for (space_counter = 0; space_counter < spaces; ++space_counter) {
+				/* Put space inf ile */
+				fputc(' ', dest_ptr);
+			}
+		}
+		else  {
+			/* Write character to file */
+			fputc(ch, dest_ptr);
+		}
+	}
+
+	fclose(source_ptr);
+	fclose(dest_ptr);
+
+	return 0;
+}
+
+void usage(void)
+{
+	fprintf(stderr, "Usage: copy-with-spaces [options] <filename> <new filename>\n");
+	fprintf(stderr, "Options\n");
+	fprintf(stderr, " 	-s<spaces> 	set number of spaces\n");
+}
