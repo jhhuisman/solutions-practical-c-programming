7d5964677366d0f7303d320d1222b57ef481f052
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-02 17:21:59 +0100

    15-5 memcpy

---
+++ chapter15/15-5/mymemcpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void *mymemcpy(void *dest, const void *src, size_t n_bytes);
+
+int main()
+{
+	char source[] = "Source text";
+	char destination[100];
+
+	mymemcpy(destination, source, (sizeof(source) / sizeof(source[0])));
+
+	printf("## %s\n", destination);
+
+	return 0;
+}
+
+void *mymemcpy(void *dest, const void *src, size_t n_bytes)
+{
+	/* Typecast src and dest to char (1 byte) */
+	char *cdest = (char *)dest;
+	char *csrc = (char *)src;
+
+	int counter;
+
+	for (counter = 0; counter < n_bytes; ++counter) {
+		cdest[counter] = csrc[counter];
+	}
+
+	return cdest;
+}
